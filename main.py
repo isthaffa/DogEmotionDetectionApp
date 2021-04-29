315e27ed1db1cf478ac92022a3b2680fb5dddf70
Ahmed Isthaffa <64108575+isthaffa@users.noreply.github.com>
2021-04-29 14:51:24 +0530

    ch

--- main.py
+++ main.py
@@ -222,4 +222,4 @@ def predictImage(image_path):
     return data
 
 if __name__=="__main__":
-    app.run(debug=True,host='127.0.0.1',port=8080)
+    app.run(debug=False,host='127.0.0.1',port=8080)
